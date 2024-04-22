37d8c6f300cbe80006c9457995447427aa02687c
aidanar <aidanarfer@outlook.es>
2024-04-22 11:31:15 +0200

    add ejercicio 2.12

--- js/main.js
+++ js/main.js
@@ -14,14 +14,14 @@ const tasks = [
   ];
 
 function renderTask(task){
-    return tasksUl.innerHTML += `<li>${task}</li>`;
-    
+    return tasksUl.innerHTML += `<li><input class="js_checkbox" type="checkbox" value="${task.completed}"> ${task.name}</li>`;
   }
 
+  
 
 const taskArray = () =>{
     for (const chore of tasks){
-        renderTask(chore.name);
+        renderTask(chore);
     }
 }
 
@@ -32,26 +32,52 @@ const allLi = document.querySelectorAll('li');
 
 // console.log(allLi);
 
-function strikeLi(){
-    for (const chore of tasks){
+// function strikeLi(){
+//     for (const chore of tasks){
         
-        if(chore.completed === true){
+//         if(chore.completed === true){
             
-            const taskIndex = tasks.indexOf(chore);
-            // console.log(`Esta tarea está completa` + tasks.indexOf(chore));
+//             const taskIndex = tasks.indexOf(chore);
+//             // console.log(`Esta tarea está completa` + tasks.indexOf(chore));
 
-            const doneLi = allLi[taskIndex];
+//             const doneLi = allLi[taskIndex];
 
-            // console.log(doneLi);
+//             // console.log(doneLi);
             
-            doneLi.classList.add('tachado');
-        }
+//             doneLi.classList.add('tachado');
+//         }
         
+//     }
+// }
+
+const checkbox = document.querySelectorAll(".js_checkbox");
+
+console.log(checkbox);
+
+function strikeLi(array){
+  // for (const item of checkbox) {
+  //   if (item.value === "true") {
+  //     item.checked = true;
+
+  //     // const taskIndex = checkbox.indexOf(item);
+  //     // console.log(taskIndex);
+  //     // const doneLi = allLi[taskIndex];
+  //     // doneLi.classList.add("tachado");
+  //   }
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].value === "true") {
+      array[i].checked = true;
+      allLi[i].classList.add("tachado");
+    } else {
+      allLi[i].classList.remove("tachado");
     }
+    console.log(array[i].value);
+  }
+
 }
 
-strikeLi();
 
-// Recorre los li, cuando incluyan chore.comleted === true, li.classList.add('tachado')
 
-// Recorre las tareas, si tiene true, 
+// strikeLi(checkbox);
+
+
