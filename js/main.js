0f187bef0494219a1e4d216d0791e3c1b4bd4696
aidanar <aidanarfer@outlook.es>
2024-04-24 10:08:33 +0200

    add codigo simplificado

--- js/main.js
+++ js/main.js
@@ -7,21 +7,45 @@ const filterInput = document.querySelector('.js-text-task-filter');
 // const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
 const SERVER_URL = `https://dev.adalab.es/api/todo`;
 
-
 let tasks = [];
-let taskLi = '';
+
+fetch(SERVER_URL)
+.then((response)=> response.json())
+.then((data)=>{
+  tasks = data.results;
+  console.log(tasks);
+  taskArray();
+  const checkboxes = document.querySelectorAll(".js_checkbox");
+  // let checkboxesArray = [];
+
+  // for(const box of checkboxes){
+  //   checkboxesArray.push(box);
+  // }
+  
+  const handleCheckbox = (event) =>{
+    completeToggle(event);
+  }
+
+  for(const box of checkboxes){
+    box.addEventListener('click', handleCheckbox);
+  }
+  
+});
+
 
 function renderTask(task){
+  let taskLi = '';
   if (task.completed === true ){
-    taskLi = `<li class="tachado"><input class="js_checkbox" type="checkbox" value="${task.completed}" id="${}" checked> ${task.name}</li>`;
+    taskLi = `<li class="tachado"><input class="js_checkbox" type="checkbox" value="${task.completed}" id="${task.id}" checked> ${task.name}</li>`;
   }else{
-    taskLi = `<li><input class="js_checkbox" type="checkbox" value="${task.completed}"> ${task.name}</li>`;
+    taskLi = `<li><input class="js_checkbox" type="checkbox" id="${task.id}" value="${task.completed}"> ${task.name}</li>`;
   }
      return taskLi;
      
   }
 
   const taskArray = () =>{
+    tasksUl.innerHTML = "";
     for (const task of tasks){
       tasksUl.innerHTML += renderTask(task);
     }
@@ -41,52 +65,48 @@ function renderTask(task){
 //2. findIndex buscar la posición en el array tasks
 //3. con la posición del array le cambio la propiedad de completed a ese array array[posición].completed = nuevo valor
 
-function completeToggle(event, arrayBtns, arrayData){
-  const boxClicked = event.target;
-  const liClicked = boxClicked.parentNode;
-  const index = arrayBtns.indexOf(boxClicked);
-  
-  if(boxClicked.value === 'true'){
-      arrayData[index].completed = false;
-       boxClicked.value = false;
-       liClicked.classList.remove('tachado');
-       boxClicked.checked = false;
-      }else{
-        arrayData[index].completed = true;
-        boxClicked.value = true;
-        liClicked.classList.add('tachado');
-        boxClicked.checked = true;
-      }
+function completeToggle(event){
+  // tasksUl.innerHTML = "";
+  const targetId = parseInt(event.target.id);
 
-      console.log(arrayData[index]);
+  const taskClicked = tasks.findIndex((task) => task.id === targetId);
+  
+    // if (tasks[taskClicked].completed === true) {
+    //   tasks[taskClicked].completed = false;
+
+    // } else {
+    //   tasks[taskClicked].completed = true;
+
+    // }
+   tasks[taskClicked].completed = !tasks[taskClicked].completed;
+  //  taskArray(tasks);
+    
+    console.log(renderTask(tasks[taskClicked]));
+
+  // const boxClicked = event.target;
+  // const liClicked = boxClicked.parentNode;
+  // const index = arrayBtns.indexOf(boxClicked);
+  
+  // if(boxClicked.value === 'true'){
+  //     arrayData[index].completed = false;
+  //      boxClicked.value = false;
+  //      liClicked.classList.remove('tachado');
+  //      boxClicked.checked = false;
+  //     }else{
+  //       arrayData[index].completed = true;
+  //       boxClicked.value = true;
+  //       liClicked.classList.add('tachado');
+  //       boxClicked.checked = true;
+  //     }
+
+  //     console.log(arrayData[index]);
 }
 
+taskArray(tasks);
 
 
 
-fetch(SERVER_URL)
-.then((response)=> response.json())
-.then((data)=>{
-  tasks = data.results;
-  console.log(tasks);
-  taskArray();
-  const checkboxes = document.querySelectorAll(".js_checkbox");
-  let checkboxesArray = [];
 
-  for(const box of checkboxes){
-    checkboxesArray.push(box);
-  }
-  
-  const handleClick = (event) =>{
-    completeToggle(event, checkboxesArray, tasks);
-  }
-
-  for(const box of checkboxes){
-    box.addEventListener('click', handleClick);
-  }
-  console.log(checkboxesArray);
-
-});
 
 
 
